4c530dba3d33d2cf9a466466966ac5f293237b18
Will Ginsberg <will.j.ginsberg@gmail.com>
2018-07-23 14:22:58 -0400

    Add static folder

--- server.js
+++ server.js
@@ -127,5 +127,8 @@ app.get('/img/rollercoaster.gif', (req, res) => res.sendFile(__dirname + '/img/r
 app.get('/img/rollercoaster2.gif', (req, res) => res.sendFile(__dirname + '/img/rollercoaster2.gif'))
 app.get('/img/ethereum.gif', (req, res) => res.sendFile(__dirname + '/img/ethereum.gif'))
 
+// Certbot
+app.use(express.static('static'))
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
