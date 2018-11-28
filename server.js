4fcf321bdcac56d519dc6a516c03e24c49168e92
Will Ginsberg <will.j.ginsberg@gmail.com>
2018-11-28 11:43:59 -0500

    Fix db connection config for production

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const config = configs[client]
 
 if (!local) {
     if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
-        config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+        config.host = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
     }
 }
 
